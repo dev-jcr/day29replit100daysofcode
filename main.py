e725145f59bada8e64e47996c68f676f4ece582a
jocarrojas <97867812+jocarrojas@users.noreply.github.com>
2023-06-07 00:45:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# end and sep in print
+import os, time
+
+def newPrint(color, word):
+  if color =="red":
+    print("\033[31m", word, sep="", end="")
+  elif color=="purple":
+    print("\033[1;35m", word, sep="", end="")
+  elif color =="cyan":
+    print("\033[0;36m", word, sep="", end="")
+  else:
+    print("\033[0m", word, sep="", end="")
+    
+print("Super subroutine\n")
+print("With my " , end="")
+newPrint("purple","new program")
+newPrint("reset", " I can just call red(\"and\") ")
+newPrint("red", "it will print in red ")
+newPrint("cyan", "or even cyan.\n\n")
+newPrint("reset","With no")
+newPrint("cyan", " weird gaps")
